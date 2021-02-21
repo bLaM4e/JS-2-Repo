cda65e95f8aea099afc2e851bf52a8c3bb63c267
Dmitry <konnerspz@mail.ru>
2021-02-21 11:01:43 +0300

    code with lesson_2

--- project/js/main.js
+++ project/js/main.js
@@ -1,25 +1,53 @@
-const products = [
-    {title: 'Notebook', id: 1, price: 2000,},
-    {title: 'Keyboard', id: 2, price: 200},
-    {title: 'Mouse', id: 3, price: 100},
-    {title: 'Gamepad', id: 4, price: 87},
-];
+class Catalog {
+    data = [];
+    // products = [];
+    container = null;
 
-// поскольку элементы приезжают из базы данных, то id будет в любом случае, да и название должно бы тоже быть, 
-// но вдруг все поля могут быть пустыми в таблице
-const renderProduct = ({ title = 'Add the product name', price }, img = 'https://placehold.it/200x150') => {
-    return `<div class="product-item">
-                <img src="${img}" alt="${title}">
-                <div class="desc">
-                    <h3>${title}</h3>
-                    <p>${price}</p>
-                    <button class="buy-btn">Купить</button>
-                </div>
-            </div>`
-};
+    constructor(selector) {
+        this.container = document.querySelector(selector);
+        this._fetchData();
+        this._render();
+    }
 
-const render = (products) => {
-    document.querySelector(".products").innerHTML = products.map(item => renderProduct(item)).join('');
-};
+    _fetchData() {
+        this.data = [
+            {title: 'Notebook', id: 1, price: 2000,},
+            {title: 'Keyboard', id: 2, price: 200},
+            {title: 'Mouse', id: 3, price: 100},
+            {title: 'Gamepad', id: 4, price: 87},
+        ];
+    }
 
-render(products);
+    _render () {
+        for (let element of this.data) {
+            const product = new Product(element);
+            // this.products.push(product);
+            this.container.insertAdjacentHTML('beforeend', product.render());
+        }
+    }
+}
+
+class Product {
+    title = '';
+    price = 0;
+    id = 0;
+    img = '';
+
+    constructor(data, img='https://placehold.it/200x150') {
+        ({title: this.title, price: this.price, id: this.id} = data);
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="product-item">
+                <img src=${this.img} alt=${this.title}>
+                    <div class="desc">
+                        <h3>${this.title}</h3>
+                        <p>${this.price}</p>
+                        <button class="buy-btn">Купить</button>
+                    </div>
+                </div>`
+    }
+}
+
+const catalog = new Catalog('.products');
