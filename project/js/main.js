a8591db45cc38420f50ba0d3129cdfe8c6f772c9
Dmitry <konnerspz@mail.ru>
2021-02-23 13:00:56 +0300

    code with lesson_3

--- project/js/main.js
+++ project/js/main.js
@@ -1,56 +1,81 @@
-class Catalog {
-    data = [];
+const API = `https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses`;
+
+let getRequest = (url, cb) => {
+    let xhr = new XMLHttpRequest();
+    // window.ActiveXObject -> new ActiveXObject();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== 4) {
+            return;
+        }
+
+        if (xhr.status !== 200) {
+            console.log('some error');
+            return;
+        }
+
+        cb(xhr.responseText);
+    }
+};
+
+
+class Products {
     products = [];
     container = null;
 
     constructor(selector) {
         this.container = document.querySelector(selector);
-        this._fetchData();
-        this._render();
+        this._fetchData()
+            .then(() => this._render());
     }
 
-    calculationSum() {
+    calcSum() {
         return this.products.reduce((accum, item) => accum += item.price, 0);
     }
 
     _fetchData() {
-        this.data = [
-            {title: 'Notebook', id: 1, price: 2000,},
-            {title: 'Keyboard', id: 2, price: 200},
-            {title: 'Mouse', id: 3, price: 100},
-            {title: 'Gamepad', id: 4, price: 87},
-        ];
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .then(data => {
+                for (let product of data) {
+                    this.products.push(new ProductItem(product));
+                }
+            })
     }
 
-    _render () {
-        for (let element of this.data) {
-            const product = new ProductCatalog(element);
-            this.products.push(product);
-            this.container.insertAdjacentHTML('beforeend', product.render());
+    _render() {
+        for (let product of this.products) {
+            if (product.rendered) {
+                continue;
+            }
+
+            this.container.insertAdjacentHTML('beforeend', product.render())
         }
     }
 }
 
-class ProductCatalog {
+class ProductItem {
     title = '';
     price = 0;
     id = 0;
     img = '';
+    rendered = false;
 
-    constructor(data, img='https://placehold.it/200x150') {
-        ({title: this.title, price: this.price, id: this.id} = data);
+    constructor(product, img = 'https://placehold.it/200x150') {
+        ({ product_name: this.title, price: this.price, id_product: this.id } = product);
         this.img = img;
     }
 
     render() {
+        this.rendered = true;
         return `<div class="product-item">
-                <img src=${this.img} alt=${this.title}>
-                    <div class="desc">
-                        <h3>${this.title}</h3>
-                        <p>${this.price}</p>
-                        <button class="buy-btn">Купить</button>
-                    </div>
-                </div>`
+                 <img src="${this.img}" alt="${this.title}">
+                 <div class="desc">
+                     <h3>${this.title}</h3>
+                     <p>${this.price}</p>
+                     <button class="buy-btn">Купить</button>
+                 </div>
+             </div>`
     }
 }
 
@@ -95,5 +120,5 @@ class Payment {
     // класс проводит оплату корзины
 }
 
-const catalog = new Catalog('.products');
-console.log(catalog.calculationSum());
+const list = new Products('.products');
+console.log(list.calcSum());
